2497695aed4fd11a257c2e0fa22a25d9137bd597
erynet <ery1423>
2015-07-31 14:30:34 +0900

    .

--- imhm/models/__init__.py
+++ imhm/models/__init__.py
@@ -6,20 +6,6 @@ from hardwares import Hardwares
 from sensors import Sensors
 from generic_values import GenericValues
 from generic_parameters import GenericParameters
-from smart_hdd_warning import HDDSmartWarning
-from smart_ssd_warning import SSDSmartWarning
+from smart_hdd_warning import SmartHDDWarning
+from smart_ssd_warning import SmartSSDWarning
 from system_warning import SystemWarning
-
-
-#from notification import Notification
-#from user import (Operator, OperatorAdminMatch, InterestAddress)
-#from order import Order
-#from request import (RequestWithdraw, RequestCharge)
-#from point import Point, PointPending
-#from address import Address
-#from price import Price
-#from office import OfficeLocation
-#from admin_code import AdminCode
-#from event import Event
-#from warning_log import  WarningLog
-#from environ import Environ
