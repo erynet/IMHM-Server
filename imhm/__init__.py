be5bf8ec3c58deef4376a412463f6709c058a4fd
erynet <ery1423>
2015-07-31 16:08:43 +0900

    .

--- imhm/__init__.py
+++ imhm/__init__.py
@@ -83,7 +83,7 @@ def create_app():
     app.config["SESSION_TYPE"] = config[config_name].SESSION_TYPE
     flask_session_obj.init_app(app)
 
-    from imhm.controllers import (base_blueprint, )
+    from imhm.controllers import (base_blueprint, manage_blueprint)
 
     # register blueprints.
     blueprints = [v for k, v in locals().items()
